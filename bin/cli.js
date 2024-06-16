be7d2a2f0436cb64b6fc51b8bba6d735995cc8f8
yuri bueno <yuribueno@live.com>
2024-06-16 04:16:24 -0400

    feat cli command and package

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,25 @@
 #!/usr/bin/env node
 const { execSync } = require("child_process");
 
-function runCommand(Command) {}
+function runCommand(command) {
+  try {
+    execSync(`${command}`, { stdio: "inherit" });
+  } catch (e) {
+    console.error(`Failed to execute ${command}`, e);
+    return false;
+  }
+  return true;
+}
 
 const repoName = process.argv[2];
 
-const gitCheckoutCommand = `git clone --depth 1  ${repoName}`;
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/yuri-bueno/base-api-yuri-starter ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+const checkedOut = runCommand(gitCheckoutCommand);
+console.log("Clonando repositorio");
+if (!checkedOut) process.exit(-1);
+console.log("Instalando dependecias");
+const installedDeps = runCommand(installDepsCommand);
+if (!installedDeps) process.exit(-1);
+console.log("Base montada com sucesso!");
