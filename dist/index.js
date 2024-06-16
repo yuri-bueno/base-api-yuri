0c883995a5681e37132b193b56642bed48025edd
yuri bueno <yuribueno@live.com>
2024-06-16 04:34:28 -0400

    chore: rebuild

--- dist/index.js
+++ dist/index.js
@@ -7,4 +7,3 @@ const router_1 = require("./modules/router");
 Object.defineProperty(exports, "Router", { enumerable: true, get: function () { return router_1.Router; } });
 const index_1 = require("./utils/index");
 Object.defineProperty(exports, "log", { enumerable: true, get: function () { return index_1.log; } });
-new core_1.appCore({ port: 3000, cors: {} }).init();
--- dist/modules/router.d.ts
+++ dist/modules/router.d.ts
@@ -1,7 +1,65 @@
-import { InewRouter } from "../@types/router";
+/// <reference types="node" />
 import { Collection } from "@discordjs/collection";
+import { Imiddleware } from "./core";
+export interface InewRouterGet {
+    id?: string;
+    path: string;
+    method: "get";
+    params?: {
+        query?: {
+            [key: string]: Iparams;
+        };
+        params?: {
+            [key: string]: Iparams;
+        };
+    };
+    middlewares?: Imiddleware[];
+    execute: (req: Request, res: Response) => void;
+}
+export interface InewRouterAllmethods {
+    id?: string;
+    path: string;
+    method: "post" | "put" | "patch" | "delete";
+    params?: {
+        query?: {
+            [key: string]: Iparams;
+        };
+        body?: {
+            [key: string]: Iparams;
+        };
+        params?: {
+            [key: string]: Iparams;
+        };
+    };
+    middlewares?: Imiddleware[];
+    execute: (req: Request, res: Response) => void;
+}
+declare type InewRouter = InewRouterGet | InewRouterAllmethods;
+export type Iparams = IBooleanParams | IStringParams | INumberParams | IArrayParams;
+interface IStringParams {
+    type: "string";
+    max?: number;
+    min?: number;
+    regex?: RegExp;
+    required?: boolean;
+}
+interface IArrayParams {
+    type: "array";
+    required?: boolean;
+}
+interface INumberParams {
+    type: "number";
+    max?: number;
+    min?: number;
+    required?: boolean;
+}
+interface IBooleanParams {
+    type: "boolean";
+    required?: boolean;
+}
 export declare class Router {
     routeConfig: InewRouter;
     static all: Collection<string, InewRouter>;
     constructor(routeConfig: InewRouter);
 }
+export {};
