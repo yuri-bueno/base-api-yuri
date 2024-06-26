16eeb392bf661c8b5842d4ee6d9fc3d3a1ccb7d7
yuri bueno <yuri.bueno@live.com>
2024-06-26 19:50:39 -0300

    feat: export express in index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { appCore, IAppCore } from "./core/index";
 import { IRouter, Route } from "./core/router";
+
+import express from "express";
 import apiErrors from "./utils/erros";
 
 import { log } from "./utils/index";
 
-export { apiErrors, appCore, IAppCore, IRouter, log, Route };
+export { apiErrors, appCore, express, IAppCore, IRouter, log, Route };
